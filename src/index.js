d86e617e076d21603a0a899b21a3c456400bf1a5
avid21 <noel@getmiso.com>
2018-01-22 16:37:20 +0900

    make components public

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import connectToState from "./connectToState";
-import CCF from "./CreditCardInput";
-import LiteCCF from "./LiteCreditCardInput";
-import CV from "./CardView";
+import _CreditCardInput from "./CreditCardInput";
+import _CardView from "./CardView";
+import _LiteCreditCardInput from "./LiteCreditCardInput";
 
-export const CreditCardInput = connectToState(CCF);
-export const LiteCreditCardInput = connectToState(LiteCCF);
-export const CardView = CV;
+import _connectToState from "./connectToState";
+
+import _CCInput from "./CCInput";
+import _Icons from "./Icons";
+
+export const CreditCardInput = _connectToState(_CreditCardInput);
+export const LiteCreditCardInput = _connectToState(_LiteCreditCardInput);
+export const CardView = _CardView;
+
+export const connectToState = _connectToState;
+export const CCInput = _CCInput;
+export const Icons = _Icons;
